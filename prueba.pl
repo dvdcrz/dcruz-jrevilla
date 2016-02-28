24b4bfda1bf7c943268b294c342cdf8213698b10
David Cruz <david.cruz@bec.seguridad.unam.mx>
2016-02-28 16:24:17 -0600

    se crea un hash que tiene ocmo llave class+sip+protocol y como valor un hash con id_evento, n_eventos, primero y ultimo

--- prueba.pl
+++ prueba.pl
@@ -4,19 +4,36 @@ use Data::Dumper;
 
 $UF_Data = openSnortUnified(shift);
 %incidentes=();
+$id=0;
 
  while ( $record = readSnortUnified2Record() ) 
 {
-
-	#print($record->{'TYPE'});
-	#print($record->$TYPE);
-
+	
 	if($record->{TYPE} == 7)
 	{
-			$incidentes{$record->{'class'}}{$record->{'sip'}}{$record->{'protocol'}}++;
+		if(exists($incidentes{$record->{'class'},$record->{'sip'},$record->{'protocol'}}))
+		{
+			$incidentes{$record->{'class'},$record->{'sip'},$record->{'protocol'}}{'n_eventos'}++;
+			$incidentes{$record->{'class'},$record->{'sip'},$record->{'protocol'}}{'ultimo'} = $record;
+
+		}
+		else
+		{
+			$incidentes{$record->{'class'},$record->{'sip'},$record->{'protocol'}}={'id_evento' => ++$id,'n_eventos' => 1,'primero' => $record, 'ultimo' => $record}; 
+		}
+			#print "$record->{'class'} $record->{'sip'} $record->{'protocol'}\n";
+			#$incidentes{$record->{'class'}}{$record->{'sip'}}{$record->{'protocol'}}++;
 			#print("entro");
 	}		
 } 
+foreach $key (keys %incidentes)
+{
+	print "key : $key  \n";
+	for $dentro (keys $incidentes{$key})
+	{
+		print "$dentro  : $incidentes{$key}{$dentro}\n";
+	}
+}
 print (Dumper(%incidentes));
 closeSnortUnified();
 

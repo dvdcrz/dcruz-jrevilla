7dd432d12a0e8821fbeb72f5a80b61d89a680455
David Cruz <david.cruz@bec.seguridad.unam.mx>
2016-03-01 14:26:14 -0600

    se corrigio errores de sintaxis

--- generaeventos.pl
+++ generaeventos.pl
@@ -1,14 +1,15 @@
+#!/usr/bin/perl
 use SnortUnified(qw(:ALL)); 
-UF_Data = openSnortUnified(shift);
+$UF_Data = openSnortUnified(shift);
 
 
 open($salida, '>>:raw','eventosgenerados')or die "no se pudo abrir $!";
  while ( $record = readSnortUnified2Record() ) 
 {
 	open($salida, '>:raw','eventosgenerados')or die "no se pudo abrir $!";
-	print salida pack('NN',$record{TYPE},$record{SIZE}).$record{raw_data};
+	print $salida pack('NN',$record{TYPE},$record{SIZE}).$record{raw_record};
 	$record = readSnortUnified2Record();
-	print salida pack('NN',$record{TYPE},$record{SIZE}).$record{raw_data};
+	print $salida pack('NN',$record{TYPE},$record{SIZE}).$record{raw_record};
 	close($salida);
 
 	sleep(30);

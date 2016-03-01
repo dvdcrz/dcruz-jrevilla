918e23d3aa600605420d0d23dc1e09ca8e9ad1d1
David Cruz <david.cruz@bec.seguridad.unam.mx>
2016-03-01 16:11:44 -0600

    este si genera eventos cada 5 segundos en el archivo eventos generados

--- generaeventos.pl
+++ generaeventos.pl
@@ -3,15 +3,14 @@ use SnortUnified(qw(:ALL));
 $UF_Data = openSnortUnified(shift);
 
 
-open($salida, '>>:raw','eventosgenerados')or die "no se pudo abrir $!";
  while ( $record = readSnortUnified2Record() ) 
 {
-	open($salida, '>:raw','eventosgenerados')or die "no se pudo abrir $!";
-	print $salida pack('NN',$record{TYPE},$record{SIZE}).$record{raw_record};
+	open($salida, '>>:unix','eventosgenerados')or die "no se pudo abrir $!";
+	print $salida pack('NN',$record->{TYPE},$record->{SIZE}).$record->{raw_record};
 	$record = readSnortUnified2Record();
-	print $salida pack('NN',$record{TYPE},$record{SIZE}).$record{raw_record};
+	print $salida pack('NN',$record->{TYPE},$record->{SIZE}).$record->{raw_record};
 	close($salida);
 
-	sleep(30);
+	sleep(5);
 
 }
